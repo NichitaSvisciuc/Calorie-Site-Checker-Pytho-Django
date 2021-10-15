a69725f637422d1af3dedf70b6f3c691f13a3a9b
nichita <57572149+saddsuppreme@users.noreply.github.com>
2021-10-16 02:24:10 +0300

    Update admin.py

--- calorie/admin.py
+++ calorie/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+
+	list_filter = ['name', 'value']
+	search_fields = ['name', 'value']
+
+admin.site.register(Item, ItemAdmin)
 admin.site.register(Order)
 admin.site.register(OrderItem)
